9b2ddb51de6bd61c43fe4a0246e7cfdc4fc2738f
Dmitriy Sobolev <disobolev@icloud.com>
2015-10-28 14:58:40 +0300

    async/await in tests

--- test.js
+++ test.js
@@ -4,51 +4,48 @@ import Queue from './';
 
 global.Promise = pinkiePromise;
 
-test('not delaying first request', a => {
+test('not delaying first request', async t => {
 	const queue = new Queue(500);
 	const start = Date.now();
 
-	return queue.up().then(() => {
-		const finish = Date.now();
-		a.ok(finish - start < 100);
-	});
+	await queue.up();
+
+	t.ok(Date.now() - start < 100);
 });
 
-test('delaying second request', a => {
-	const queue = new Queue(500);
-	const start = Date.now();
-
-	queue.up();
-
-	return queue.up().then(() => {
-		const finish = Date.now();
-		a.ok(finish - start > 450 && finish - start < 550);
-	});
-});
-
-test('delaying third request', a => {
+test('delaying second request', async t => {
 	const queue = new Queue(500);
 	const start = Date.now();
 
 	queue.up();
+
+	await queue.up();
+
+	const finish = Date.now();
+	t.ok(finish - start > 450 && finish - start < 550);
+});
+
+test('delaying third request', async t => {
+	const queue = new Queue(500);
+	const start = Date.now();
+
+	queue.up();
 	queue.up();
 
-	return queue.up().then(() => {
-		const finish = Date.now();
-		a.ok(finish - start > 950 && finish - start < 1050);
-	});
+	await queue.up();
+
+	const finish = Date.now();
+	t.ok(finish - start > 950 && finish - start < 1050);
 });
 
-test('passing value', a => {
-	return (new Queue()).up(1337).then(val => {
-		a.is(val, 1337);
-	});
+test('passing value', async t => {
+	const value = await (new Queue()).up(1337);
+	t.is(value, 1337);
 });
 
-test('custom Promise module', a => {
+test('custom Promise module', async t => {
 	const queue = new Queue(500, pinkiePromise);
 
-	return queue.up().then(() => {
-		a.pass();
-	});
+	await queue.up();
+	t.pass();
 });
